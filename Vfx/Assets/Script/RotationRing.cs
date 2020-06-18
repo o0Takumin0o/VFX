713686ddd1b351a95a0c1ec66d36ec76e1fe691a
o0Takumin0o <dark.damon.14@gmail.com>
2020-06-18 23:29:30 +0700

    vfx update

---
+++ Vfx/Assets/Script/RotationRing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationRing : MonoBehaviour
+{
+    public Vector3 speed;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+    }
+
+}
---
+++ Vfx/Assets/Script/magicBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magicBullet : MonoBehaviour
+{
+    public GameObject canonball;
+    public float shootForce = 0f;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            GameObject projectile = (GameObject)Instantiate
+                (canonball, transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody>().AddForce(projectile.transform.forward * shootForce);
+        }
+    }
+}
